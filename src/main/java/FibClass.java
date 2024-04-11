977ebbbc92fc6c41ae2e496a87017cab64deb000
Himal Shrestha <Shresthahimal12@yahoo.com>
2024-04-10 23:26:31 -0400

    - created the algorithim which continues to find the needed triangle number until it is greater then 100. and then the loop stops after finding the number and prints it out.

--- src/main/java/FibClass.java
+++ src/main/java/FibClass.java
@@ -17,13 +17,13 @@ public class FibClass {
     }
 
     static int fibIter(int n) {
-       
-        int i, f[] = new int[n + 2]; 
+
+        int i, f[] = new int[n + 2];
         f[0] =f[1]= 0;
-       
-        for (i = 2; i <= n; i++) 
+
+        for (i = 2; i <= n; i++)
             f[i] = f[i - 1] + f[i - 2];
-        
+
         return f[n];
     }
 
